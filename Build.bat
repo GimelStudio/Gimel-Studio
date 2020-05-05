54fa3941713efc8fdc9fea2139d5fa68e3cd1dc7
Correct Syntax <correctsyntax@yahoo.com>
2020-05-05 13:23:41 -0500

    updated filepath for icon

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 pyinstaller -n "Gimel Studio"^
                 --noconsole^
 	--hidden-import pkg_resources.py2_warn^
-	-i "src/GIMELSTUDIO_ICO.ico"^
+	-i "assets/GIMELSTUDIO_ICO.ico"^
 	"src/Gimel Studio.py"
