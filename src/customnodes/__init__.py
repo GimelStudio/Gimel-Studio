e375318b8f005d380c7382226860d648f8adfbe5
Correct Syntax <correctsyntax@yahoo.com>
2020-08-30 15:03:20 -0500

    start implementation of add text node

--- src/customnodes/__init__.py
+++ src/customnodes/__init__.py
@@ -3,7 +3,7 @@
 # Add the filename (without the .py extenstion) to the below list to make
 # your custom node(s) available for registering.
 
-__all__ = ['example_custom_node', 'tut_custom_node']
+__all__ = ['example_custom_node']
 
 
 
