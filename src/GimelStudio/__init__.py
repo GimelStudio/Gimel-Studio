bf9b23488fbdcc18956aeb450a66458f772f655d
Correct Syntax <correctsyntax@yahoo.com>
2020-09-20 11:54:57 -0500

    add full version string constant

--- src/GimelStudio/__init__.py
+++ src/GimelStudio/__init__.py
@@ -1,9 +1,11 @@
 from .meta import (
     APP_NAME, APP_AUTHOR,
     APP_VERSION, APP_DEBUG,
-    APP_TITLE
+    APP_TITLE, FULL_APP_VERSION_STRING
     )
 from .file_support import (
     SupportFTOpen,
     SupportFTSave
     )
+
+
