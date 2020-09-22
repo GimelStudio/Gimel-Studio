110521d60b6ba756255dc2e98c9600f951a04673
Correct Syntax <correctsyntax@yahoo.com>
2020-09-22 14:10:16 -0500

    reimplement color balance node

--- src/GimelStudio/corenodes/color/color_balance_node.py
+++ src/GimelStudio/corenodes/color/color_balance_node.py
@@ -15,89 +15,52 @@
 ## limitations under the License.
 ## ----------------------------------------------------------------------------
 
-import wx
-from PIL import Image, ImageEnhance
+from PIL import ImageEnhance
 
-from GimelStudio.api import (Color, RenderImage, List, NodeBase, 
-                            Parameter, Property, RegisterNode)
+from GimelStudio import api
 
   
-class NodeDefinition(NodeBase):
-    
-    @property
-    def NodeIDName(self):
-        return "gimelstudiocorenode_colorbalance"
+class ColorBalanceNode(api.NodeBase):
+    def __init__(self, _id):
+        api.NodeBase.__init__(self, _id)
 
     @property
-    def NodeLabel(self):
-        return "Color Balance"
+    def NodeMeta(self):
+        meta_info = {
+            "label": "Color Balance",
+            "author": "Correct Syntax",
+            "version": (1, 1, 0),
+            "supported_app_version": (0, 5, 0),
+            "category": "COLOR",
+            "description": "Adjusts the image color balance.",
+        }
+        return meta_info
 
-    @property
-    def NodeCategory(self):
-        return "COLOR"
-
-    @property
-    def NodeDescription(self):
-        return "Adjusts the image color balance." 
-
-    @property
-    def NodeVersion(self):
-        return "1.1" 
-
-    @property
-    def NodeAuthor(self):
-        return "Correct Syntax Software" 
-
-    @property
-    def NodeProperties(self):
-        return [
-            Property('Amount',
-                prop_type='INTEGER',
-                value=1
-                ),
-            ]
-
-    @property
-    def NodeParameters(self):
-        return [
-            Parameter('Image',
-                param_type='RENDERIMAGE',
-                default_value=RenderImage('RGBA', (256, 256), (0, 0, 0, 1))
-                ),
-        ]
-
-   
-    def NodePropertiesUI(self, node, parent, sizer):
-        current_amount_value = self.NodeGetPropValue('Amount')
-
-        colorbalance_label = wx.StaticText(parent, label="Color balance amount:")
-        sizer.Add(colorbalance_label, border=5)
-
-        self.colorbalance_slider = wx.Slider(
-            parent, wx.ID_ANY, 
-            value=current_amount_value,
-            minValue=1, maxValue=10,
-            style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS
+    def NodeInitProps(self):
+        p = api.PositiveIntegerProp(
+            idname="Amount", 
+            default=1, 
+            min_val=1, 
+            max_val=25, 
+            widget=api.SLIDER_WIDGET,
+            label="Amount:",
             )
-        self.colorbalance_slider.SetTickFreq(1)
+        self.NodeAddProp(p)
 
-        sizer.Add(self.colorbalance_slider, flag=wx.EXPAND|wx.ALL, border=5)
+    def NodeInitParams(self):
+        p = api.RenderImageParam('Image')
 
-        parent.Bind(wx.EVT_SCROLL_THUMBRELEASE, self.OnColorBalanceChange, self.colorbalance_slider)
+        self.NodeAddParam(p)
 
- 
-    def OnColorBalanceChange(self, event):
-        self.NodePropertiesUpdate('Amount', self.colorbalance_slider.GetValue())
-    
     def NodeEvaluation(self, eval_info):
         image1  = eval_info.EvaluateParameter('Image')
-        contrast_amount = eval_info.EvaluateProperty('Amount')
+        amount = eval_info.EvaluateProperty('Amount')
 
-        image = RenderImage()
+        image = api.RenderImage()
         enhancer = ImageEnhance.Color(image1.GetImage())
-        image.SetAsImage(enhancer.enhance(contrast_amount).convert('RGBA'))
+        image.SetAsImage(enhancer.enhance(amount).convert('RGBA'))
         self.NodeSetThumb(image.GetImage())
         return image
 
- 
-RegisterNode(NodeDefinition)
+
+api.RegisterNode(ColorBalanceNode, "corenode_colorbalance")
