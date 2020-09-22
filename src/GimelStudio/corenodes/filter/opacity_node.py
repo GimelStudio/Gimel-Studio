319a4cfc885a6a2fea4c6f1ff47f8e92448b1bc9
Correct Syntax <correctsyntax@yahoo.com>
2020-09-22 14:11:42 -0500

    reimplement opacity node

--- src/GimelStudio/corenodes/filter/opacity_node.py
+++ src/GimelStudio/corenodes/filter/opacity_node.py
@@ -15,80 +15,43 @@
 ## limitations under the License.
 ## ----------------------------------------------------------------------------
 
-import wx
-import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import ImageEnhance 
 
-from GimelStudio.api import (Color, RenderImage, List, NodeBase, 
-                            Parameter, Property, RegisterNode)
+from GimelStudio import api
 
  
-class NodeDefinition(NodeBase):
-    
-    @property
-    def NodeIDName(self):
-        return "gimelstudiocorenode_opacity"
+class OpacityNode(api.NodeBase):
+    def __init__(self, _id):
+        api.NodeBase.__init__(self, _id)
 
     @property
-    def NodeLabel(self):
-        return "Opacity"
+    def NodeMeta(self):
+        meta_info = {
+            "label": "Opacity",
+            "author": "Correct Syntax",
+            "version": (1, 1, 0),
+            "supported_app_version": (0, 5, 0),
+            "category": "FILTER",
+            "description": "Reduces the image transparency/opacity.",
+        }
+        return meta_info
 
-    @property
-    def NodeCategory(self):
-        return "FILTER"
-
-    @property
-    def NodeDescription(self):
-        return "Reduces the image transparency/opacity." 
-
-    @property
-    def NodeVersion(self):
-        return "1.0" 
-
-    @property
-    def NodeAuthor(self):
-        return "Correct Syntax Software" 
-
-    @property
-    def NodeProperties(self):
-        return [
-            Property('Opacity',
-                prop_type='INTEGER',
-                value=50
-                ),
-            ]
-
-    @property
-    def NodeParameters(self):
-        return [
-            Parameter('Image',
-                param_type='RENDERIMAGE',
-                default_value=RenderImage('RGBA', (256, 256), (0, 0, 0, 1))
-                ),
-        ]
-
-   
-    def NodePropertiesUI(self, node, parent, sizer):
-        current_opacity_value = self.NodeGetPropValue('Opacity')
-
-        opacity_label = wx.StaticText(parent, label="Image opacity:")
-        sizer.Add(opacity_label, border=5)
-
-        self.opacity_slider = wx.Slider(
-            parent, 100, 25, 1, 100,
-            style=wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS
+    def NodeInitProps(self):
+        p = api.PositiveIntegerProp(
+            idname="Opacity", 
+            default=50, 
+            min_val=0, 
+            max_val=100, 
+            widget=api.SLIDER_WIDGET,
+            label="Opacity:",
             )
-        self.opacity_slider.SetTickFreq(5)
-        self.opacity_slider.SetRange(1, 100)
-        self.opacity_slider.SetValue(current_opacity_value)
-        sizer.Add(self.opacity_slider, flag=wx.EXPAND|wx.ALL, border=5)
+        self.NodeAddProp(p)
 
-        parent.Bind(wx.EVT_SCROLL_THUMBRELEASE, self.OnOpacityChange, self.opacity_slider)
+    def NodeInitParams(self):
+        p = api.RenderImageParam('Image')
 
- 
-    def OnOpacityChange(self, evt):
-        self.NodePropertiesUpdate('Opacity', self.opacity_slider.GetValue())
-    
+        self.NodeAddParam(p)
+
     def NodeEvaluation(self, eval_info):
         image1  = eval_info.EvaluateParameter('Image')
         opacity = eval_info.EvaluateProperty('Opacity')
@@ -103,10 +66,10 @@ class NodeDefinition(NodeBase):
             alpha = ImageEnhance.Brightness(img.split()[-1]).enhance(image_opacity)
             img.putalpha(alpha)
         
-        image = RenderImage()
+        image = api.RenderImage()
         image.SetAsImage(img)
         self.NodeSetThumb(image.GetImage())
         return image
 
- 
-RegisterNode(NodeDefinition)
+
+api.RegisterNode(OpacityNode, "corenode_opacity")
