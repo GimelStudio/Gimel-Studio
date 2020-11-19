1233f76b501b5f0aa192ed10aede8c16fe4b0739
Correct Syntax <correctsyntax@yahoo.com>
2020-11-19 16:27:40 -0600

    add Dilate/Erode node

---
+++ src/GimelStudio/corenodes/filter/dilate_erode_node.py
@@ -0,0 +1,108 @@
+## THIS FILE IS A PART OF GIMEL STUDIO AND IS LICENSED UNDER THE SAME TERMS:
+## ----------------------------------------------------------------------------
+## Gimel Studio Copyright 2019-2020 by Noah Rahm and contributors
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## ----------------------------------------------------------------------------
+
+import cv2
+
+from GimelStudio import api
+from GimelStudio.utils.image import ArrayFromImage, ArrayToImage
+
+
+class DilateErodeNode(api.NodeBase):
+    def __init__(self, _id):
+        api.NodeBase.__init__(self, _id)
+
+    @property
+    def NodeMeta(self):
+        meta_info = {
+            "label": "Dilate/Erode",
+            "author": "Correct Syntax",
+            "version": (0, 5, 0),
+            "supported_app_version": (0, 5, 0),
+            "category": "FILTER",
+            "description": "Applies a morphological operation like Erosion, Dilation, Opening, Closing etc.",
+        }
+        return meta_info
+
+    def NodeInitProps(self):
+        operation = api.ChoiceProp(
+            idname="Operation",
+            default="Erode",
+            choices=["Dilate", "Erode", "Opening", "Closing", "Top Hat", "Black Hat"],
+            label="Operation:"
+        )
+        kernel_shape = api.ChoiceProp(
+            idname="Kernel Shape",
+            default="Rectangle",
+            choices=["Rectangle", "Ellipse", "Cross"],
+            label="Kernel Shape:"
+        )
+        kernel_size = api.PositiveIntegerProp(
+            idname="Kernel Size",
+            default=5,
+            min_val=0,
+            max_val=100,
+            widget=api.SLIDER_WIDGET,
+            label="Kernel Size:"
+        )
+        self.NodeAddProp(operation)
+        self.NodeAddProp(kernel_shape)
+        self.NodeAddProp(kernel_size)
+
+    def NodeInitParams(self):
+        image = api.RenderImageParam('Image')
+
+        self.NodeAddParam(image)
+
+    def NodeEvaluation(self, eval_info):
+        image1 = eval_info.EvaluateParameter('Image')
+        kernel_shape = eval_info.EvaluateProperty('Kernel Shape')
+        kernel_size = eval_info.EvaluateProperty('Kernel Size')
+        operation = eval_info.EvaluateProperty('Operation')
+
+        image = api.RenderImage()
+
+        img = ArrayFromImage(image1.GetImage())
+
+        if kernel_shape == "Rectangle":
+            kshape = cv2.MORPH_RECT
+        elif kernel_shape == "Ellipse":
+            kshape = cv2.MORPH_ELLIPSE
+        elif kernel_shape == "Cross":
+            kshape = cv2.MORPH_CROSS
+
+        kernel_img = cv2.getStructuringElement(kshape, (kernel_size, kernel_size))
+
+        if operation == "Erode":
+            output_img = cv2.erode(img, kernel_img, iterations=1)
+        elif operation == "Dilate":
+            output_img = cv2.dilate(img, kernel_img, iterations=1)
+        elif operation == "Opening":
+            output_img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel_img)
+        elif operation == "Closing":
+            output_img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel_img)
+        elif operation == "Top Hat":
+            output_img =  cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel_img)
+        elif operation == "Black Hat":
+            output_img =  cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel_img)
+
+        image.SetAsImage(ArrayToImage(output_img).convert("RGBA"))
+
+        self.NodeSetThumb(image.GetImage())
+        return image
+
+
+api.RegisterNode(DilateErodeNode, "corenode_dilateerode")
--- src/GimelStudio/node_importer.py
+++ src/GimelStudio/node_importer.py
@@ -21,7 +21,7 @@
 
 from GimelStudio.corenodes.input import (image_node, color_image_node,
                                         noise_image_node, gradient_image_node)
-#from GimelStudio.corenodes.mask import  # add nodes here
+#from GimelStudio.corenodes.mask import edge_detect_node
 #from GimelStudio.corenodes.draw import text_node
 from GimelStudio.corenodes.color import (color_balance_node, contrast_node,
                                         brightness_node, invert_alpha_node,
@@ -31,7 +31,7 @@ from GimelStudio.corenodes.blend import (mix_node, composite_node,
 from GimelStudio.corenodes.distort import flip_node #resize_node, rotate_node
 from GimelStudio.corenodes.filter import (blur_node, opacity_node,
                                          sharpness_node, effect_spread_node,
-                                         invert_node)
+                                         invert_node, dilate_erode_node)
 from GimelStudio.corenodes.convert import (to_normal_map_node, to_bump_map_node,
                                            to_roughness_map_node,
                                            to_specular_map_node, to_ao_map_node)
