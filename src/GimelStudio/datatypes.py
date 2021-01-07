4698a1341d581559cc5fc0e1b8dd7bdd8e647269
Correct Syntax <correctsyntax@yahoo.com>
2021-01-06 19:44:17 -0600

    edit to use openimageio for io

--- src/GimelStudio/datatypes.py
+++ src/GimelStudio/datatypes.py
@@ -18,29 +18,35 @@
 # PURPOSE: Define the main data types
 # ----------------------------------------------------------------------------
 
+import cv2
+import numpy as np
 from PIL import Image
+import OpenImageIO as oiio
+from OpenImageIO import ImageBuf, ImageSpec, ImageBufAlgo
+
+from GimelStudio.utils import ArrayFromImage, ArrayToImage
 
 
 class RenderImage(object):
     """ Represents an image data type for the renderer. """
 
-    def __init__(self, size=(100, 100), color=(0, 0, 0, 1), packed_data=None):
-        self._img = Image.new("RGBA", (size[0], size[1]), color)
+    def __init__(self, size=(100, 100), packed_data=None):
+        self._img = np.zeros((size[0], size[1], 4), dtype=np.uint8)
         self._packedData = packed_data
-
-    def GetPILImage(self):
-        """ Returns the image.
-
-        :returns: PIL ``Image`` object
-        """
-        return self._img
 
     def GetImage(self):
         """ Returns the image.
 
-        :returns: PIL ``Image`` object
+        :returns: ``numpy.ndarray`` object
         """
         return self._img
+
+    def GetPILImage(self):
+        """ Returns the image as a PIL Image.
+
+        :returns: PIL ``Image`` object
+        """
+        return ArrayToImage(self._img)
 
     def SetAsOpenedImage(self, path):
         """ Sets the image and opens it. If the image is non-existent,
@@ -49,7 +55,15 @@ class RenderImage(object):
         :param path: image filepath to be opened
         """
         try:
-            self._img = Image.open(path)
+            # Open the image as an array
+            img_input = oiio.ImageInput.open(path)
+            image = img_input.read_image(format="uint8")
+
+            # Enforce RGBA
+            if image.shape[2] == 3:
+                self._img = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
+            else:
+                self._img = image
         except FileNotFoundError:
             if self._packedData is not None:
                 self._img = self._packedData
@@ -57,8 +71,32 @@ class RenderImage(object):
                 print("WARNING: COULD NOT GET PACKED IMAGE DATA!")
 
     def SetAsImage(self, image):
-        """ Sets the image.
+        """ Sets the render image and converts it to the correct datatype.
+
+        :param image: ``numpy.ndarray``, Pillow ``Image`` object
+        """
+        img_type = type(image)
+
+        # if img_type == Image:
+        #     self._img = ArrayFromImage(image)
+        # elif img_type == ImageBuf:
+        #     self._img = ImageBuf.get_pixels(image)
+        # elif img_type == np.ndarray:
+        self._img = image
+
+    def SetAsImageFromPIL(self, image):
+        """ Sets the image from a PIL Image.
 
         :param image: PIL ``Image`` object
         """
-        self._img = image
+        self._img = ArrayFromImage(image)
+
+
+    # def AsImgType(self, output_type="NDARRAY"):
+    #     img_type = type(self._img)
+    #     print(img_type)
+
+    #     if output_type
+    #     image =
+
+    #     return
